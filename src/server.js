b8dcb59fb0a9572fa394c589677521d5ed212cc2
Artur Poffo <arturpoffoc@hotmail.com>
2024-07-24 16:29:07 -0300

    First Commit

---
+++ src/server.js
@@ -0,0 +1,49 @@
+import { faker } from "@faker-js/faker";
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import cors from "cors";
+
+const app = express();
+
+app.use(cors())
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+function generateAiChat() {
+  const aiMessages = []
+
+  for (let i = 0; i < 5; i++) {
+    const agentMessage = {
+      agent: `Agent ${i + 1}`,
+      task: faker.lorem.words(4),
+      data: faker.lorem.paragraphs(3)
+    }
+
+    aiMessages.push(agentMessage);
+  }
+
+  return aiMessages
+}
+
+io.on("connection", (socket) => {
+  console.log('a user connected');
+  
+  const aiChat = generateAiChat();
+
+  socket.emit('generated ai chat', aiChat)
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
+
+server.listen(3001, () => {
+  console.log('server running at http://localhost:3001');
+});
